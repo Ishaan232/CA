c023218d7c25664d3d5797416e181d65a37f5260
Ishaan232 <83758075+Ishaan232@users.noreply.github.com>
2021-12-29 11:15:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var a =document.getElementById("nput1");
+var b =document.getElementById("nput2");
+var c =document.getElementById("nput3")
+var d=document.getElementById("nput4")
+function add(){
+    var fullname =a.concat(b);
+    setText("product", c*d);
+    setText("name", fullname);
+}
+
+
+
